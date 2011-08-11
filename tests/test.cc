4d159a31d8ddc589f6f21f776f3ac88c4d5c3ec9
Dmitry Matveev <me@dmitrymatveev.co.uk>
2011-08-11 02:57:16 +0000

    Started working on inotify behavioral tests

---
+++ tests/test.cc
@@ -0,0 +1,466 @@
+#include <cstddef> // NULL
+#include <cstdint> // uint32_t
+#include <cstring> // memset
+#include <cstdlib> // system
+#include <cassert>
+#include <ctime>
+#include <string>
+#include <set>
+#include <algorithm> // find_if
+#include <pthread.h>
+#include <sys/inotify.h>
+#include <poll.h>
+#include <iostream>
+
+#define LOG(X)                                               \
+    std::cout                                                \
+    << static_cast<unsigned int>(pthread_self ()) << "    "  \
+    << X                                                     \
+    << std::endl
+
+struct event {
+    std::string filename;
+    uint32_t watch;
+    uint32_t flags;
+
+    event (const std::string &filename_ = "", uint32_t watch_ = 0, uint32_t flags_ = 0);
+    bool operator< (const event &ev) const;
+};
+
+event::event (const std::string &filename_, uint32_t watch_, uint32_t flags_)
+: filename (filename_)
+, watch (watch_)
+, flags (flags_)
+{
+}
+
+bool event::operator< (const event &ev) const
+{
+    // yes, compare by a filename and watch id only
+    return watch != ev.watch ? watch < ev.watch : filename < ev.filename;
+}
+
+typedef std::set<event> events;
+
+class event_by_name {
+    std::string look_for;
+
+public:
+    event_by_name (const std::string &name);
+    bool operator() (const event &ev) const;
+};
+
+event_by_name::event_by_name (const std::string &name)
+: look_for (name)
+{
+}
+
+bool event_by_name::operator() (const event &ev) const
+{
+    return ev.filename == look_for;
+}
+
+class action {
+    pthread_barrier_t barrier;
+    void init();
+
+    const std::string name;
+    volatile bool interrupted;
+
+public:
+    action (const std::string &name_);
+    virtual ~action () = 0;
+    
+    bool wait ();
+    void interrupt ();
+    void reset ();
+};
+
+action::action (const std::string &name_)
+: name (name_)
+, interrupted (false)
+{
+    init ();
+}
+
+action::~action ()
+{
+}
+
+void action::init ()
+{
+    // initialize the barrier for two threads: a producer and a consumer
+    pthread_barrier_init (&barrier, NULL, 2);
+    interrupted = false;
+}
+
+bool action::wait ()
+{
+    pthread_barrier_wait (&barrier);
+    return !interrupted;
+}
+
+void action::interrupt ()
+{
+    interrupted = true;
+    wait ();
+}
+
+void action::reset ()
+{
+    pthread_barrier_destroy (&barrier);
+    init ();
+}
+
+
+class request: public action {
+public:
+    enum variant {
+        REGISTER_ACTIVITY,
+        ADD_MODIFY_WATCH,
+        REMOVE_WATCH,
+    };
+
+    struct activity {
+        events expected;
+        unsigned int timeout;
+    };
+
+    struct add_modify {
+        std::string path;
+        uint32_t mask;
+    };
+
+    struct remove {
+        int watch_id;
+    };
+
+private:
+    // Not a union, because its members are non-POD
+    struct {
+        activity _act;
+        add_modify _am;
+        remove _rm;
+    } variants;
+
+    variant current;
+    
+public:
+    request ();
+    void setup (const events &expected, unsigned int timeout = 0);
+    void setup (const std::string &path, uint32_t mask);
+    void setup (uint32_t rm_id);
+    
+    variant current_variant () const;
+    activity activity_data () const;
+    add_modify add_modify_data () const;
+    remove remove_data () const;
+};
+
+request::request ()
+: action ("REQUEST")
+{
+}
+
+void request::setup (const events &expected, unsigned int timeout)
+{
+    current = REGISTER_ACTIVITY;
+    variants._act.expected = expected;
+    variants._act.timeout = timeout;
+    wait ();
+}
+
+void request::setup (const std::string &path, uint32_t mask)
+{
+    current = ADD_MODIFY_WATCH;
+    variants._am.path = path;
+    variants._am.mask = mask;
+    wait ();
+}
+
+void request::setup (uint32_t rm_id)
+{
+    current = REMOVE_WATCH;
+    variants._rm.watch_id = rm_id;
+    wait ();
+}
+
+request::variant request::current_variant () const
+{
+    return current;
+}
+
+request::activity request::activity_data () const
+{
+    assert (current == REGISTER_ACTIVITY);
+    return variants._act;
+}
+
+request::add_modify request::add_modify_data () const
+{
+    assert (current == ADD_MODIFY_WATCH);
+    return variants._am;
+}
+
+request::remove request::remove_data () const
+{
+    assert (current == REMOVE_WATCH);
+    return variants._rm;
+}
+
+
+
+class response: public action {
+public:
+    enum variant {
+        UNREGISTERED_EVENTS,
+        WATCH_ID,
+    };
+
+private:
+    // Again, not a union
+    struct {
+        events _left_unreg;
+        uint32_t _watch_id;
+    } variants;
+
+    variant current;
+
+public:
+    response ();
+    void setup (const events &unregistered);
+    void setup (uint32_t watch_id);
+
+    variant current_variant () const;
+    events left_unregistered () const;
+    uint32_t added_watch_id () const;
+};
+
+response::response ()
+: action ("RESPONSE")
+{
+}
+
+void response::setup (const events &unregistered)
+{
+    current = UNREGISTERED_EVENTS;
+    variants._left_unreg = unregistered;
+    wait ();
+}
+
+void response::setup (uint32_t watch_id)
+{
+    current = WATCH_ID;
+    variants._watch_id = watch_id;
+    wait ();
+}
+
+response::variant response::current_variant () const
+{
+    return current;
+}
+
+events response::left_unregistered () const
+{
+    assert (current == UNREGISTERED_EVENTS);
+    return variants._left_unreg;
+}
+
+uint32_t response::added_watch_id () const
+{
+    assert (current == WATCH_ID);
+    return variants._watch_id;
+}
+
+
+class consumer;
+
+class producer {
+    consumer *cons;
+    std::string path;
+
+public:
+    void assign (consumer *cons_);
+};
+
+void producer::assign (consumer *cons_)
+{
+    assert (cons_ != NULL);
+    cons = cons_;
+}
+
+
+class inotify_client {
+    int fd;
+
+public:
+    inotify_client ();
+    uint32_t watch (const std::string &filename, uint32_t flags);
+    void cancel (uint32_t watch_id);
+    bool get_next_event (event &ev, int timeout) const;
+};
+
+inotify_client::inotify_client()
+: fd (inotify_init())
+{
+    assert (fd != -1);
+}
+
+uint32_t inotify_client::watch (const std::string &filename, uint32_t flags)
+{
+    assert (fd != -1);
+    return inotify_add_watch (fd, filename.c_str(), flags);
+}
+
+void inotify_client::cancel (uint32_t watch_id)
+{
+    assert (fd != -1);
+    inotify_rm_watch (fd, watch_id);
+}
+
+#define IE_BUFSIZE ((sizeof (struct inotify_event) + FILENAME_MAX))
+
+bool inotify_client::get_next_event (event& ev, int timeout) const
+{
+    struct pollfd pfd;
+    memset (&pfd, 0, sizeof (struct pollfd));
+    pfd.fd = fd;
+    pfd.events = POLLIN;
+
+    poll (&pfd, 1, timeout * 1000);
+
+    if (pfd.revents & POLLIN) {
+        char buffer[IE_BUFSIZE];
+        read (fd, buffer, IE_BUFSIZE);
+
+        struct inotify_event *ie = (struct inotify_event *) buffer;
+        ev.filename = ie->name;
+        ev.flags = ie->mask;
+        ev.watch = ie->wd;
+        return true;
+    }
+
+    return false;
+}
+
+
+class consumer {
+    std::string path;
+    inotify_client ino;
+
+    // yes, these methods take arguments by a value. intentionally
+    void register_activity (request::activity activity);
+    void add_modify_watch (request::add_modify add_modify);
+    void remove_watch (request::remove remove);
+   
+public:
+    request input;
+    response output;
+
+    consumer ();
+    static void* run_ (void *ptr);
+    void run ();
+};
+
+void* consumer::run_ (void *ptr)
+{
+    assert (ptr != NULL);
+    ((consumer *) ptr)->run();
+    return NULL;
+}
+
+consumer::consumer ()
+{
+    pthread_t pid;
+    pthread_create (&pid, NULL, consumer::run_, this);
+}
+
+void consumer::register_activity (request::activity activity)
+{
+    // Now we have a set of events which should occur on a watched entity.
+    // Receive events until the timeout will be reached.
+    time_t start = time (NULL);
+    time_t elapsed = 0;
+    
+    while (!activity.expected.empty()
+           && (elapsed = time (NULL) - start) < activity.timeout) {
+        event ev;
+        if (ino.get_next_event (ev, activity.timeout)) {
+            event_by_name matcher (ev.filename);
+            events::iterator it = std::find_if (activity.expected.begin(), activity.expected.end(), matcher); 
+            if (it != activity.expected.end()) {
+                activity.expected.erase (it);
+            }
+        }
+    }
+
+    // Inform producer about results
+    input.reset ();
+    output.setup (activity.expected);
+}
+
+void consumer::add_modify_watch (request::add_modify add_modify)
+{
+    uint32_t id = ino.watch (add_modify.path, add_modify.mask);
+    input.reset ();
+    output.setup (id);
+}
+
+void consumer::remove_watch (request::remove remove)
+{
+    ino.cancel (remove.watch_id);
+    input.reset ();
+    output.wait ();
+}
+
+void consumer::run ()
+{
+    while (input.wait ()) {
+        switch (input.current_variant ()) {
+        case request::REGISTER_ACTIVITY:
+            register_activity (input.activity_data ());
+            break;
+
+        case request::ADD_MODIFY_WATCH:
+            add_modify_watch (input.add_modify_data ());
+            break;
+
+        case request::REMOVE_WATCH:
+            remove_watch (input.remove_data ());
+            break;
+        }
+    }
+}
+
+int main (int argc, char *argv[]) {
+    producer prod;
+    consumer cons;
+    uint32_t wid = 0;
+
+    prod.assign (&cons);
+
+    cons.input.setup ("/tmp/foo", IN_ATTRIB);
+    cons.output.wait ();
+    wid = cons.output.added_watch_id ();
+    cons.output.reset ();
+
+    events expected;
+    expected.insert (event("1", wid, IN_ATTRIB));
+    expected.insert (event("2", wid, IN_ATTRIB));
+    cons.input.setup (expected, 1);
+
+    system ("touch /tmp/foo/1");
+    system ("touch /tmp/foo/2");
+    
+    cons.output.wait ();
+    expected = cons.output.left_unregistered ();
+    cons.output.reset ();
+    
+    for (events::iterator it = expected.begin(); it != expected.end(); ++it) {
+        std::cout << '[' << it->watch << "] " << it->filename << std::endl;
+    }
+
+    cons.input.interrupt ();
+    
+    return 0;
+}
